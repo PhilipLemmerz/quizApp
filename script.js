871d7b385147315bd0286fb27edb4dd20ee785ca
Philip Lemmerz <plemmerz@yahoo.de>
2021-10-19 10:35:51 +0200

    JSON-Complete

---
+++ script.js
@@ -0,0 +1,67 @@
+let questions = [
+
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer1": "Robbie Williams",
+        "answer2": "Lady Gaga",
+        "answer3": "Tim Berners-Lee",
+        "answer4": "Justin Bieber",
+        "rightAnswer": 3
+    },
+    {   
+        "question": "Was bedeutet das HTML <a> Tag?",
+        "answer1": "fetter Text",
+        "answer2": "Ein Container",
+        "answer3": "Ein Link",
+        "answer4": "kurisver Text",
+        "rightAnswer": 3
+    },
+    {   
+        "question": "Wie bindet mann eine Website in eine Website ein?",
+        "answer1": "<iframe>, <frame>, <frameset>",
+        "answer2": "<iframe>",
+        "answer3": "<frame>",
+        "answer4": "<frameset>",
+        "rightAnswer": 2
+    },
+    {   
+        "question": "Was bedeutet das HTML <a> Tag?",
+        "answer1": "fetter Text",
+        "answer2": "Ein Container",
+        "answer3": "Ein Link",
+        "answer4": "kurisver Text",
+        "rightAnswer": 3
+    },
+    {   
+        "question": "Wie stellt man Text fett dar?",
+        "answer1": "<em> Tag",
+        "answer2": "<b> Tag",
+        "answer3": "<i> Tag",
+        "answer4": "<strong> Tag",
+        "rightAnswer": 4
+    },
+    {   
+        "question": "Welches Atttribut kann man nicht für <textarea> verwenden",
+        "answer1": "readonly",
+        "answer2": "max",
+        "answer3": "from",
+        "answer4": "spellcheck",
+        "rightAnswer": 1
+    },
+    {   
+        "question": "Wie selektierst du alle <a> Elemente die ein 'title' Attribut haben",
+        "answer1": "a[title] {...}",
+        "answer2": "a>[title]{...}",
+        "answer3": "a.title{...}",
+        "answer4": "a=title{...}",
+        "rightAnswer": 1
+    },
+    {   
+        "question": "Wie definiert man in JavaScript eine Variable?",
+        "answer1": "let 100 = rate;",
+        "answer2": "let rate = 100;",
+        "answer3": "rate = 100;",
+        "answer4": "100 = let rate;",
+        "rightAnswer": 2
+    },
+]
